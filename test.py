f0880af757ba09d878f6861e69aa7be508c46003
Billy Dongdong Sun <51125175+ds199911@users.noreply.github.com>
2022-01-17 00:15:10 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import requests
 
 # https://your-heroku-app-name.herokuapp.com/predict
 # http://localhost:5000/predict
-resp = requests.post("https://testingapicv.herokuapp.com/predict", files={'file': open('Cat_1.jpeg', 'rb')})
+resp = requests.post("https://testingapicv.herokuapp.com/predict", files={'file': open('mask1.jpeg', 'rb')})
 
 print(resp.text)
